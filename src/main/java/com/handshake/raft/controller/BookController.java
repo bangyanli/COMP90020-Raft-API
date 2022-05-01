52538dda081d52d8b07ef487e1373edd43ebebe1
bangyanli <bangyan2015@gmail.com>
2022-05-01 19:28:21 +1000

    Revert "add required raft classes"

--- src/main/java/com/handshake/raft/controller/BookController.java
+++ src/main/java/com/handshake/raft/controller/BookController.java
@@ -3,11 +3,6 @@ package com.handshake.raft.controller;
 import com.handshake.raft.common.response.ResponseResult;
 import com.handshake.raft.dao.BookInfo;
 import com.handshake.raft.service.BookService;
-import com.handshake.raft.service.LibraryService;
-import io.swagger.annotations.ApiImplicitParam;
-import io.swagger.annotations.ApiImplicitParams;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.http.MediaType;
@@ -59,7 +54,6 @@ public class BookController {
     public ResponseResult<Object> uploadBookChapter(@PathVariable("name") String name,
                                                     @PathVariable("chapter") String chapter,
                                                     @RequestPart(name = "file" ,required = true) MultipartFile file){
-
         bookService.uploadChapter(name,chapter,file);
         return ResponseResult.suc("Upload chapter successfully!", null);
     }
